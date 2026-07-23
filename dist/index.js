e20f166cd5ea59480d7d2f4ba5d2fec6c1ddced3
tatyna <tatyna.absatarova@axelkg.com>
2026-07-23 16:59:38 +0600

    fix: gitignore

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const selector_1 = require("./entypoint/selector");
-const entryPoint = (0, selector_1.selectEntryPoint)();
-entryPoint.run();
